b6161696e14c29c79ae28a46dd51edf2d7be8e5f
George Ho <george.h.w.ho@gmail.com>
2021-03-23 09:11:18 -0400

    Add route to garden/new

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -2,4 +2,8 @@ class GardensController < ApplicationController
   def index
     @gardens = %w[1 2 3]
   end
+
+  def new
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/gardens', to: 'gardens#index'
-
+  get '/gardens/new', to: 'gardens#new'
 end
