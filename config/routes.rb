80adbcaf0f4ddd51e02c22555602058bf99a144c
George Ho <george.h.w.ho@gmail.com>
2021-03-23 17:42:47 -0400

    Add user story 2 for garden

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get '/gardens', to: 'gardens#index'
   get '/gardens/new', to: 'gardens#new'
   post '/gardens', to: 'gardens#create'
-  get '/garden/:id', to: 'gardens#show'
+  get '/gardens/:id', to: 'gardens#show'
 end
