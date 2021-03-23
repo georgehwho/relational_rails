e4afd7739487df539f8361ff60a851f528a0fc4e
George Ho <george.h.w.ho@gmail.com>
2021-03-23 08:54:52 -0400

    Update garden schema and model

--- db/migrate/20210323123725_create_gardens.rb
+++ db/migrate/20210323123725_create_gardens.rb
@@ -1,9 +1,11 @@
-class CreateGarden < ActiveRecord::Migration[5.2]
+class CreateGardens < ActiveRecord::Migration[5.2]
   def change
     create_table :gardens do |t|
+      t.string :name
       t.boolean :watered
       t.integer :max_plant_capacity
-      t.string :name
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_011614) do
+ActiveRecord::Schema.define(version: 2021_03_23_123725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "gardens", force: :cascade do |t|
+    t.string "name"
     t.boolean "watered"
     t.integer "max_plant_capacity"
-    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
