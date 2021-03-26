e9184de7282b8d502baa0b26b97734153f76d4e7
Austin <>
2021-03-26 15:57:33 -0400

    Create game_companies video_games test & update show for formatting

--- spec/features/game_companies/video_games_spec.rb
+++ spec/features/game_companies/video_games_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "video_games show page", type: :feature do
-  it "can see selected video game by id's attributes" do
+RSpec.describe "game_companies video_games page", type: :feature do
+  it "can see selected game company by id's video game collection" do
     game_company = GameCompany.create!(title: "Bethesda", profitable: true, employees: 60000)
     video_game_1 = VideoGame.create!(name:        "GTA",
                                     max_players: 500000,
@@ -11,10 +11,21 @@ RSpec.describe "video_games show page", type: :feature do
                                     max_players: 2300000,
                                     is_live:     false,
                                     game_company_id: game_company.id)
-    visit "/video_games/#{video_game_1.id}"
+    visit "/game_companies/#{game_company.id}/video_games"
 
+    expect(page).to have_content(game_company.video_games.ids.sort == [video_game_1.id, video_game_2.id])
     expect(page).to have_content(video_game_1.name)
     expect(page).to have_content(video_game_1.max_players)
     expect(page).to have_content(video_game_1.is_live)
+    expect(page).to have_content(video_game_1.id)
+    expect(page).to have_content(game_company.id)
+
+    first(:link, 'Details').click
+
+    expect(page).to have_content(video_game_2.name)
+    expect(page).to have_content(video_game_2.max_players)
+    expect(page).to have_content(video_game_2.is_live)
+    expect(page).to have_content(video_game_2.id)
+    expect(page).to have_content(game_company.id)
   end
 end
