ae66ee58f9d7bf7a26506f73f885c2f3e80f0a35
Austin <>
2021-03-25 22:52:27 -0400

    Create gardens index test

--- spec/features/gardens/index_spec.rb
+++ spec/features/gardens/index_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "gardens index page", type: :feature do
   it "can see all gardens and their creation date" do
-    garden = Garden.create!(name: "Blue rose", watered: true, max_plant_capacity: 25)
-    plant_1 = Plant.create!(name:        "GTA",
+    garden = Garden.create!(name: "Backstreet", watered: true, max_plant_capacity: 25)
+    plant_1 = Plant.create!(name:        "Blue Rose",
                             age: 5,
                             in_season:     true,
                             garden_id: garden.id)
-    plant_2 = Plant.create!(name:        "League of Legends",
+    plant_2 = Plant.create!(name:        "Yellow Flower",
                             age: 10,
                             in_season:     false,
                             garden_id: garden.id)
@@ -18,21 +18,21 @@ RSpec.describe "gardens index page", type: :feature do
     # expect(page).to have_content("created #{time_ago_in_words(garden.created_at)} ago")
   end
 
-  it "can redirect to a plant's attributes page" do
-    garden = Garden.create!(name: "Red rose", watered: false, max_plant_capacity: 10)
-    plant_1 = Plant.create!(name:        "GTA",
-                            age: 2,
+  it "can see all gardens and their creation date" do
+    garden = Garden.create!(name: "Front Lawn", watered: true, max_plant_capacity: 25)
+    plant_1 = Plant.create!(name:        "Red Rose",
+                            age: 5,
                             in_season:     true,
                             garden_id: garden.id)
-    plant_2 = Plant.create!(name:        "League of Legends",
-                            age: 24,
+    plant_2 = Plant.create!(name:        "Benca",
+                            age: 10,
                             in_season:     false,
                             garden_id: garden.id)
     visit "/gardens"
 
-    expect(current_path).to eq('/gardens')
-    expect(page).to have_link("#{garden.name}")
-    click_link "#{garden.name}"
-    expect(current_path).to eq("/gardens/#{garden.id}")
+  #   expect(current_path).to eq('/gardens')
+  #   expect(page).to have_link("#{garden.name}")
+  #   click_link "#{garden.name}"
+  #   expect(current_path).to eq("/gardens/#{garden.id}")
   end
 end
