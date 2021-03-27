b50319225ae3e6c2047dcb320244c9319bbc8e1c
Austin <>
2021-03-26 22:54:59 -0400

    Update routes for get to new page and post to home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get '/', to: 'welcome#index'
   get '/game_companies', to: 'game_companies#index'
+  get '/game_companies/new', to: 'game_companies#new'
+  post '/game_companies', to: 'game_companies#create'
   get '/game_companies/:id', to: 'game_companies#show'
   get '/game_companies/:id/video_games', to: 'game_companies#video_games'
 
