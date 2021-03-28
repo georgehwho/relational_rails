6f23ca2398fd19208bededd3048a9884967c4a10
Austin <>
2021-03-27 23:37:00 -0400

    Add video game addition redirection it do

--- spec/features/game_companies/video_games_spec.rb
+++ spec/features/game_companies/video_games_spec.rb
@@ -35,4 +35,28 @@ RSpec.describe "game_companies video_games page", type: :feature do
 
     expect(page).not_to have_content(video_game_3.name)
   end
+
+  it "can redirect to an add video game to current game company page" do
+    game_company = GameCompany.create!(title: "Bethesda", profitable: true, employees: 60000)
+    game_company_2 = GameCompany.create!(title: "Hollywood", profitable: true, employees: 5000)
+    video_game_1 = VideoGame.create!(name:        "GTA",
+                                    max_players: 500000,
+                                    is_live:     true,
+                                    game_company_id: game_company.id)
+    video_game_2 = VideoGame.create!(name:        "League of Legends",
+                                    max_players: 2300000,
+                                    is_live:     false,
+                                    game_company_id: game_company.id)
+    video_game_3 = VideoGame.create!(name:        "L.A",
+                                    max_players: 2300000,
+                                    is_live:     false,
+                                    game_company_id: game_company_2.id)
+    visit "/game_companies/#{game_company.id}/video_games"
+
+    expect(current_path).to eq("/game_companies/#{game_company.id}/video_games")
+    expect(page).to have_link("Add New Video Game to this Company")
+    click_link "Add New Video Game to this Company"
+    expect(current_path).to eq("/game_companies/#{game_company.id}/video_games/new")
+
+  end
 end
