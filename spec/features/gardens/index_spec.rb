f205cde870545d91dec6a075e2ca7502acb581c9
George Ho <george.h.w.ho@gmail.com>
2021-03-30 09:34:50 -0400

    Update index tests

--- spec/features/gardens/index_spec.rb
+++ spec/features/gardens/index_spec.rb
@@ -1,38 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe "gardens index page", type: :feature do
-  it "can see all gardens and their creation date" do
-    garden = Garden.create!(name: "Backstreet", watered: true, max_plant_capacity: 25)
-    plant_1 = Plant.create!(name:        "Blue Rose",
-                            age: 5,
-                            in_season:     true,
-                            garden_id: garden.id)
-    plant_2 = Plant.create!(name:        "Yellow Flower",
-                            age: 10,
-                            in_season:     false,
-                            garden_id: garden.id)
-    visit "/gardens"
+describe "gardens index page", type: :feature do
+  describe 'they visit /gardens' do
+    it "can see all gardens and their creation date" do
+      garden = Garden.create!(name: "Backstreet", watered: true, max_plant_capacity: 25)
 
-    expect(page).to have_content(garden.name)
-    expect(page).to have_content("These are the Gardens")
-    # expect(page).to have_content("created #{time_ago_in_words(garden.created_at)} ago")
+      visit "/gardens"
+
+      expect(page).to have_link(garden.name)
+      expect(page).to have_content("These are the Gardens")
+      expect(page).to have_content(garden.created_at)
+    end
   end
 
-  it "can see all gardens and their creation date" do
-    garden = Garden.create!(name: "Front Lawn", watered: true, max_plant_capacity: 25)
-    plant_1 = Plant.create!(name:        "Red Rose",
-                            age: 5,
-                            in_season:     true,
-                            garden_id: garden.id)
-    plant_2 = Plant.create!(name:        "Benca",
-                            age: 10,
-                            in_season:     false,
-                            garden_id: garden.id)
-    visit "/gardens"
+  describe 'they can create a new garden from index' do
+    it 'can navigate to /new' do
+      visit "/gardens"
 
-  #   expect(current_path).to eq('/gardens')
-  #   expect(page).to have_link("#{garden.name}")
-  #   click_link "#{garden.name}"
-  #   expect(current_path).to eq("/gardens/#{garden.id}")
+      expect(current_path).to eq("/gardens")
+
+      click_link "Create Garden"
+
+      expect(current_path).to eq("/gardens/new")
+    end
+  end
+
+  describe 'they can click on the garden names' do
+    it 'navigates to the garden show page' do
+      garden = Garden.create!(name: "Front Lawn", watered: true, max_plant_capacity: 25)
+
+      visit "/gardens"
+
+      expect(current_path).to eq('/gardens')
+      expect(page).to have_link("#{garden.name}")
+      click_link "#{garden.name}"
+      expect(current_path).to eq("/gardens/#{garden.id}")
+    end
   end
 end
