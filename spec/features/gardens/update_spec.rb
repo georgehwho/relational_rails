7ce98ae60f34447f62a8737f0ad02e4dac72f7f2
George Ho <george.h.w.ho@gmail.com>
2021-03-30 14:13:17 -0400

    Add update test template

---
+++ spec/features/gardens/update_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe 'gardens update page', type: :feature do
+  describe 'they click the garden index' do
+    describe 'they click garden name' do
+      describe 'they click on edit' do
+        describe 'they fill in new info' do
+          it 'updates the garden' do
+
+          end
+        end
+      end
+    end
+  end
+end
